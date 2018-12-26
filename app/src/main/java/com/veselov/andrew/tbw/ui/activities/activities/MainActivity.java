e268aefda11375c51122d7a18b1dcce9416b8f80
andrew veselov <andrew.veselov@gmail.com>
2018-12-26 16:00:57 +0500

    ДЗ 1. Добавить датчики температуры и влажности (TYPE_AMBIENT_TEMPERATURE, TYPE_ABSOLUTE_HUMIDITY).    Показывать текущую температуру, влажность в погодном приложении (если такой датчик присутствует    в аппарате).
    2. * Создать свой элемент View и использовать его в погодном приложении.
    
    С элементом view только поэкспериментировал, но пока не применил в приложении.

--- app/src/main/java/com/veselov/andrew/tbw/ui/activities/activities/MainActivity.java
+++ app/src/main/java/com/veselov/andrew/tbw/ui/activities/activities/MainActivity.java
@@ -1,30 +1,29 @@
 package com.veselov.andrew.tbw.ui.activities.activities;
-// Android Level 2 Lesson 1
-// Homework 20-Dec-2018
+// Android Level 2 Lesson 2
+// Homework 22-Dec-2018
 // Andrew Veselov
 //
-// 1. Создать tool bar для приложения с меню. Подумать, какие пункты меню вам нужны.
+// 1. Добавить датчики температуры и влажности (TYPE_AMBIENT_TEMPERATURE, TYPE_ABSOLUTE_HUMIDITY).
+//    Показывать текущую температуру, влажность в погодном приложении (если такой датчик присутствует
+//    в аппарате).
 //
-// 2. Добавить в проект Navigation Drawer. В Drawer добавить Header с аватаром пользователя,
-//    а также несколько пунктов: «О разработчике», «Форма обратной связи» + пункты на ваше усмотрение.
-//    При нажатии на пункты должны открываться соответствующие фрагменты с содержимым.
-//
-//3. * Добавить цветовую индикацию пунктов Drawer, чтобы всегда подсвечивался тот пункт, который выбран.
+// 2. * Создать свой элемент View и использовать его в погодном приложении.
 //
 
 import android.content.res.Configuration;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.veselov.andrew.tbw.R;
 import com.veselov.andrew.tbw.interfaces.OnWorkoutListItemSelectedListener;
+import com.veselov.andrew.tbw.ui.activities.fragments.TempAndHum;
 import com.veselov.andrew.tbw.ui.activities.fragments.WorkoutAboutFragment;
 import com.veselov.andrew.tbw.ui.activities.fragments.WorkoutDetailFragment;
 import com.veselov.andrew.tbw.ui.activities.fragments.WorkoutListFragment;
@@ -75,13 +74,22 @@ public class MainActivity extends AppCompatActivity implements OnWorkoutListItem
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        fragmentManager = getSupportFragmentManager();
         if (id == R.id.nav_about) {
-            fragmentManager = getSupportFragmentManager();
             WorkoutAboutFragment workoutAboutFragment = new WorkoutAboutFragment();
-            fragmentManager.beginTransaction().replace(R.id.port_container, workoutAboutFragment).addToBackStack(null).commit();
-        } else if (id == R.id.nav_feedback) {
-            Toast.makeText(this, "Feedback pressed.", Toast.LENGTH_SHORT).show();
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            fragmentManager.beginTransaction().replace(R.id.port_container, workoutAboutFragment).addToBackStack(null).commit();}
+            else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                fragmentManager.beginTransaction().replace(R.id.land_detail_container, workoutAboutFragment).commit();
+            }
+        } else if (id == R.id.nav_tempandhum) {
+            TempAndHum tempAndHum = new TempAndHum();
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                fragmentManager.beginTransaction().replace(R.id.port_container, tempAndHum).addToBackStack(null).commit();}
+            else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                fragmentManager.beginTransaction().replace(R.id.land_detail_container, tempAndHum).commit();
+            }
+
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
--- app/src/main/java/com/veselov/andrew/tbw/ui/activities/fragments/WorkoutAboutFragment.java
+++ app/src/main/java/com/veselov/andrew/tbw/ui/activities/fragments/WorkoutAboutFragment.java
@@ -1,4 +1,14 @@
 package com.veselov.andrew.tbw.ui.activities.fragments;
+// Android Level 2 Lesson 2
+// Homework 22-Dec-2018
+// Andrew Veselov
+//
+// 1. Добавить датчики температуры и влажности (TYPE_AMBIENT_TEMPERATURE, TYPE_ABSOLUTE_HUMIDITY).
+//    Показывать текущую температуру, влажность в погодном приложении (если такой датчик присутствует
+//    в аппарате).
+//
+// 2. * Создать свой элемент View и использовать его в погодном приложении.
+//
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
