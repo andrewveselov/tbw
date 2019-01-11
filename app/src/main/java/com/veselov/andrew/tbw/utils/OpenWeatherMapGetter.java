e60d680b132f9b7c0c01a9e357ad1f69a9a15fcd
andrew veselov <andrew.veselov@gmail.com>
2019-01-11 23:55:41 +0500

    Исправил недочеты в соответствии с замечаниями преподавателя. Выполнил ДЗ: // 1. Посвятите это время работе над вашим проектом. Ваше портфолио - это ваше лицо для будущих работодателей. // // 2. Доделайте те задания, которые не успели сделать к предыдущим урокам. // // 3*. Добавьте в ваш проект какие-нибудь новые фичи на ваше усмотрение. //

--- app/src/main/java/com/veselov/andrew/tbw/utils/OpenWeatherMapGetter.java
+++ app/src/main/java/com/veselov/andrew/tbw/utils/OpenWeatherMapGetter.java
@@ -7,18 +7,39 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class OpenWeatherMapGetter {
     private static final String OPEN_WEATHER_API_KEY = "de7fa657bb311ee51c70ead50aaf49ac";
-    private static final String OPEN_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+    private static final String OPEN_WEATHER_API_URL_CITY = "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+    private static final String OPEN_WEATHER_API_URL_LOC = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric";
     private static final String KEY = "x-api-key";
     private static final String RESPONSE = "cod";
     private static final int ALL_GOOD = 200;
 
     public static JSONObject getJSONData(Context context, String city) {
         try {
-            URL url = new URL(String.format(OPEN_WEATHER_API_URL, city));
+            URL url = new URL(String.format(OPEN_WEATHER_API_URL_CITY, city));
+            return readJSONData(url);
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            return null;
+        }
+    }
+
+    public static JSONObject getJSONData(Context context, double lat, double lon) {
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_API_URL_LOC, String.valueOf(lat),String.valueOf(lon)));
+            return readJSONData(url);
+    } catch (Exception exc) {
+            exc.printStackTrace();
+            return null;
+        }
+    }
+
+    private static JSONObject readJSONData (URL url) {
+        try {
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.addRequestProperty(KEY, OPEN_WEATHER_API_KEY);
 
