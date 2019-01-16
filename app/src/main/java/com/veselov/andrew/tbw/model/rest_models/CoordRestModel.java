07f6405986b2f0bd547d9ecccba4bb07b8c1d962
andrew veselov <andrew.veselov@gmail.com>
2019-01-16 17:22:24 +0500

    Выполнил ДЗ: // 1. Переделать запросы и обработку ответа в погодном приложении на Retrofit 2. // // 2. * Оживить погодное приложение картинками из Интернета.
    Осталось разобраться с requestLocationUpdates для нормальной работы определителя местаположения.

---
+++ app/src/main/java/com/veselov/andrew/tbw/model/rest_models/CoordRestModel.java
@@ -0,0 +1,10 @@
+package com.veselov.andrew.tbw.model.rest_models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+//"coord":{"lon":30.26,"lat":59.89},
+public class CoordRestModel {
+    @SerializedName("lon") @Expose public float lon;
+    @SerializedName("lat") @Expose public float lat;
+}
--- app/src/main/java/com/veselov/andrew/tbw/ui/activities/fragments/TempAndHum.java
+++ app/src/main/java/com/veselov/andrew/tbw/ui/activities/fragments/TempAndHum.java
@@ -1,13 +1,11 @@
 package com.veselov.andrew.tbw.ui.activities.fragments;
-// Android Level 2 Lesson 5
-// Homework 09-Jan-2019
+// Android Level 2 Lesson 6
+// Homework 12-Jan-2019
 // Andrew Veselov
 //
-// 1. Посвятите это время работе над вашим проектом. Ваше портфолио - это ваше лицо для будущих работодателей.
+// 1. Переделать запросы и обработку ответа в погодном приложении на Retrofit 2.
 //
-// 2. Доделайте те задания, которые не успели сделать к предыдущим урокам.
-//
-// 3*. Добавьте в ваш проект какие-нибудь новые фичи на ваше усмотрение.
+// 2. * Оживить погодное приложение картинками из Интернета.
 //
 
 import android.Manifest;
@@ -22,8 +20,10 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
+
+import com.google.android.gms.location.LocationListener;
+
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
@@ -32,6 +32,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import android.support.v4.app.Fragment;
@@ -40,15 +41,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
 import com.veselov.andrew.tbw.R;
+import com.veselov.andrew.tbw.model.OpenWeatherMapRepo;
+import com.veselov.andrew.tbw.model.rest_models.WeatherRequestRestModel;
 import com.veselov.andrew.tbw.utils.Constants;
-import com.veselov.andrew.tbw.utils.OpenWeatherMapGetter;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class TempAndHum extends Fragment {
@@ -56,13 +61,13 @@ public class TempAndHum extends Fragment {
     private SensorManager sensorManager;
     private Sensor sensorTemp, sensorHum;
 
-    private final Handler handler = new Handler();
-
     private TextView cityTextView;
     private TextView currentTemperatureTextView;
     private TextView weatherIconTextView;
+    private ImageView weatherServiceLogo;
     private LocationManager locationManager = null;
     private Location location;
+    private LocationListener locationListener;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -70,13 +75,11 @@ public class TempAndHum extends Fragment {
         View root = inflater.inflate(R.layout.fragment_temp_and_hum, container, false);
 
         init(root);
+        showWeatherServiceLogo(Constants.OPEN_WEATHER_LOGO_URL);
         checkSavedCity();
         setHasOptionsMenu(true);
         // Define sensor manager
         sensorManager = (SensorManager) Objects.requireNonNull(getActivity()).getSystemService(getContext().SENSOR_SERVICE);
-
-        // Define location manager and current location
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
 
         // Check temperature and humidity sensors
         assert sensorManager != null;
@@ -123,7 +126,7 @@ public class TempAndHum extends Fragment {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 String city = input.getText().toString();
-                updateWeatherData(city);
+                loadWeatherData(city);
             }
         });
 
@@ -131,18 +134,10 @@ public class TempAndHum extends Fragment {
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) ==
-                        PackageManager.PERMISSION_GRANTED) {
-                    location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-                } else {
-                    Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), R.string.no_permission_for_location, Toast.LENGTH_LONG).show();
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Constants.REQUEST_CODE_PERMISSION_FINE_LOCATION);
-                    return;
-                }
-
+                location = getLastKnownLocation();
                 if (location != null) {
 //                    Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), String.valueOf(location.getLatitude()) + " " + String.valueOf(location.getLongitude()), Toast.LENGTH_LONG).show();
-                    updateWeatherData(location.getLatitude(), location.getLongitude());
+                    loadWeatherData(location.getLatitude(), location.getLongitude());
                 } else
                     Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), R.string.city_not_determined, Toast.LENGTH_LONG).show();
             }
@@ -159,6 +154,28 @@ public class TempAndHum extends Fragment {
         sensorManager.unregisterListener(listenerHum, sensorHum);
     }
 
+    // Getting location
+    private Location getLastKnownLocation() {
+        locationManager = (LocationManager) Objects.requireNonNull(getActivity()).getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(Objects.requireNonNull(getContext()), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), R.string.no_permission_for_location, Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Constants.REQUEST_CODE_PERMISSION_FINE_LOCATION);
+        }
+        assert locationManager != null;
+        List<String> providers = locationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,30000,10, (LocationListener) this);
+            Location l = locationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
+    }
     // Init
     private void init(View view) {
         textHum = view.findViewById(R.id.sensor_hum);
@@ -166,6 +183,7 @@ public class TempAndHum extends Fragment {
         cityTextView = view.findViewById(R.id.weather_city);
         currentTemperatureTextView = view.findViewById(R.id.weather_current_temp);
         weatherIconTextView = view.findViewById(R.id.weather_icon);
+        weatherServiceLogo = view.findViewById(R.id.weather_service_logo);
         Typeface weatherFont = Typeface.createFromAsset(Objects.requireNonNull(getActivity()).getAssets(), "fonts/weather.ttf");
         weatherIconTextView.setTypeface(weatherFont);
     }
@@ -173,7 +191,7 @@ public class TempAndHum extends Fragment {
     private void checkSavedCity() {
         SharedPreferences preferences = Objects.requireNonNull(getContext()).getSharedPreferences(Constants.SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE);
         String city = preferences.getString(Constants.WEATHER_CITY, getString(R.string.default_city));
-        updateWeatherData(city);
+        loadWeatherData(city);
     }
 
     private void saveCity(String city) {
@@ -183,70 +201,56 @@ public class TempAndHum extends Fragment {
         editor.apply();
     }
 
-    private void updateWeatherData(final String city) {
-        new Thread() {
-            @Override
-            public void run() {
-                final JSONObject jsonObject = OpenWeatherMapGetter.getJSONData(Objects.requireNonNull(getActivity()).getApplicationContext(), city);
-                if (jsonObject == null) {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getActivity().getApplicationContext(), R.string.city_not_found, Toast.LENGTH_LONG).show();
-                        }
-                    });
-                } else {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            renderWeather(jsonObject);
-                        }
-                    });
-                }
-            }
-        }.start();
+    private void loadWeatherData(String city){
+        OpenWeatherMapRepo.getInstance().getAPI().loadWeatherCity(city, Constants.OPEN_WEATHER_API_KEY, Constants.OPEN_WEATHER_UNITS_METRIC)
+                .enqueue(new Callback<WeatherRequestRestModel>() {
+                    @Override
+                    public void onResponse(@NonNull Call<WeatherRequestRestModel> call,
+                                           @NonNull Response<WeatherRequestRestModel> response) {
+                            if(response.code() == Constants.CITY_NOT_FOUND)
+                                errorMessage(getString(R.string.city_not_found));
+                            else if (response.isSuccessful() && response.body() != null) renderWeather(response);
+                                 else errorMessage(getString(R.string.error_get_data));
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<WeatherRequestRestModel> call, Throwable t) {
+                        errorMessage(getString(R.string.error_get_data));
+                    }
+                });
     }
 
-    private void updateWeatherData(final double lat, final double lon) {
-        new Thread() {
-            @Override
-            public void run() {
-                final JSONObject jsonObject = OpenWeatherMapGetter.getJSONData(Objects.requireNonNull(getActivity()).getApplicationContext(), lat, lon);
-                if (jsonObject == null) {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(getActivity().getApplicationContext(), R.string.city_not_found, Toast.LENGTH_LONG).show();
-                        }
-                    });
-                } else {
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            renderWeather(jsonObject);
-                        }
-                    });
-                }
-            }
-        }.start();
+    private void loadWeatherData(double lat, double lon){
+        OpenWeatherMapRepo.getInstance().getAPI().loadWeatherLocation(lat, lon, Constants.OPEN_WEATHER_API_KEY,Constants.OPEN_WEATHER_UNITS_METRIC)
+                .enqueue(new Callback<WeatherRequestRestModel>() {
+                    @Override
+                    public void onResponse(@NonNull Call<WeatherRequestRestModel> call,
+                                           @NonNull Response<WeatherRequestRestModel> response) {
+                        if (response.code() == Constants.CITY_NOT_FOUND)
+                            errorMessage(getString(R.string.city_not_found));
+                        else if (response.isSuccessful() && response.body() != null)
+                            renderWeather(response);
+                        else errorMessage(getString(R.string.error_get_data));
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<WeatherRequestRestModel> call, Throwable t) {
+                        errorMessage(getString(R.string.error_get_data));
+                    }
+                });
     }
 
-    private void renderWeather(JSONObject jsonObject) {
-        try {
-            JSONObject details = jsonObject.getJSONArray("weather").getJSONObject(0);
-            JSONObject main = jsonObject.getJSONObject("main");
-
-            setPlaceName(jsonObject);
-            setCurrentTemp(main);
-            setWeatherIcon(details.getInt("id"),
-                    jsonObject.getJSONObject("sys").getLong("sunrise") * 1000,
-                    jsonObject.getJSONObject("sys").getLong("sunset") * 1000);
-        } catch (Exception exc) {
-            exc.printStackTrace();
-        }
+    private void renderWeather(Response<WeatherRequestRestModel> response) {
+        setPlaceName(response);
+        setCurrentTemp(response);
+        setWeatherIcon(response);
     }
 
-    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
+    private void setWeatherIcon(Response<WeatherRequestRestModel> response) {
+        assert response.body() != null;
+        int actualId = response.body().weatherRestModel[0].id;
+        long sunrise = response.body().sysRestModel.sunrise * 1000;
+        long sunset = response.body().sysRestModel.sunset * 1000;
         int id = actualId / 100;
         String icon = "";
 
@@ -288,16 +292,31 @@ public class TempAndHum extends Fragment {
         weatherIconTextView.setText(icon);
     }
 
-    private void setCurrentTemp(JSONObject main) throws JSONException {
-        String currentTextText = String.format("%.2f", main.getDouble("temp")) + "\u2103";
+    private void setCurrentTemp(Response<WeatherRequestRestModel> response) {
+        String currentTextText = String.format("%.2f", response.body().mainRestModel.temp) + "\u2103";
         currentTemperatureTextView.setText(currentTextText);
     }
 
-    private void setPlaceName(JSONObject jsonObject) throws JSONException {
-        String cityText = jsonObject.getString("name").toUpperCase() + ", "
-                + jsonObject.getJSONObject("sys").getString("country");
+    private void setPlaceName(Response<WeatherRequestRestModel> response) {
+        assert response.body() != null;
+        String cityText = response.body().name.toUpperCase() + ", "
+                + response.body().sysRestModel.country;
         cityTextView.setText(cityText);
         saveCity(cityText);
+    }
+
+    private void showWeatherServiceLogo(String url) {
+        Picasso.get()
+                .load(url)
+                .into(weatherServiceLogo);
+    }
+
+    private void errorMessage(String message){
+        // clear fields
+        cityTextView.setText("");
+        currentTemperatureTextView.setText("");
+        weatherIconTextView.setText("");
+        Toast.makeText(Objects.requireNonNull(getActivity()).getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
     // Show temperature and humidity sensor values
